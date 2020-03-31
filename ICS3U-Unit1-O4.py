bdb2606368cef1115135897289a60f51eb5cb4cd
Ubuntu <ubuntu@ip-172-31-46-83.ec2.internal>
2020-03-31 19:14:34 +0000

    commit message

---
+++ ICS3U-Unit1-O4.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python 3
+
+# Created by: Andrew Coxall
+# Created on: Mar 31, 2020
+# This is the area of a square with a length of 5 and a hight of 3 (cm)
+
+"""
+Your module description
+"""
+print("Perimeter is 5*2 + 3*2={}" .format(5*2 + 3*2))
+print("Area is 5*3={} " .format(5*3))
